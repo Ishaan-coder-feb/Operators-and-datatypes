d8c6176e6f33fc994d527a13d85df9ebe7049d0b
Ishaan-coder-feb <goyalishaan007@gmail.com>
2026-05-01 16:14:35 +0400

    Add files via upload

---
+++ PythonOperators.py
@@ -0,0 +1,17 @@
+a=6
+b=15
+sum=a+b
+dif=a-b
+pro=a*b
+quo=a/b
+flrdiv=a//b
+expo=a**b
+modu=a%b
+print("Sum is",sum)
+print("Difference is",dif)
+print("Porduct is",pro)
+print("Quotient is",quo)
+print("Floor div answer is",flrdiv)
+print("Exponential answer is",expo)
+print("Modulus answer is",modu)
+
