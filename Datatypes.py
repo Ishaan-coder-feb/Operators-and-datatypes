e3aaae95c534577642799fba72155593635c4a80
Ishaan-coder-feb <goyalishaan007@gmail.com>
2026-05-01 16:07:19 +0400

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,16 @@
+name="Ishaan"
+age=13
+weight=40.5
+std=True
+a=type(name)
+print("Data type of name is", a)
+b=type(age)
+c=type(weight)
+d=type(std)
+print("Data type of age is", b)
+print("Data type of weight is", c)
+print("Data type of student is", d)
+age=str(13)
+weight=int(40.5)
+print("The new data type for age is",type(age))
+print("The new data type for weight is",type(weight))
