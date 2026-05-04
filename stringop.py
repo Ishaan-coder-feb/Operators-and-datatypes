030537ddf331c2fc9d49fa444abe65e37658e9e3
Ishaan-coder-feb <goyalishaan007@gmail.com>
2026-05-04 21:39:38 +0400

    Add files via upload

---
+++ stringop.py
@@ -0,0 +1,14 @@
+first="Ishaan"
+second="Goyal"
+full_name=first+ second
+example=first*5
+print("First name is", first)
+print("Second name is", second)
+print("Full name is", full_name)
+print("The first name multiplied by 5 is", example)
+string="I love cricket."
+a=len(string)
+print("Length of string is",a)
+print("First character of string is",string[0])
+print("Last chacter of string is", string[13])
+print("The last word of the string is", string[7:14])
