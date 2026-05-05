ac37ad60f95d80b68bfd485a0a99735558209347
Ishaan-coder-feb <goyalishaan007@gmail.com>
2026-05-05 17:34:43 +0400

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+x=int(input("Enter value x: "))
+y=int(input("Enter value y: "))
+z=int(input("Enter value z: "))
+temp=x
+example=y
+abc=z
+y=temp
+z=example
+x=abc
+print("The numbers after swapped are")
+print(" x=",abc," y=",temp," z=",example )
---
+++ numberswap.py
@@ -0,0 +1,6 @@
+x=int(input("Enter num x: "))
+y=int(input("Enter num y: "))
+temp=x
+x=y
+y=temp
+print("After swapping x and y ,x is",x, "and y is", temp)
